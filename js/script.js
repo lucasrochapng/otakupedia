47a9b6d60284129be8851dbdc51ea425f34a9221
lucas <lucasrochadss@gmail.com>
2023-12-25 18:41:32 -0300

    atts cards

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
+//! HEADER
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const menuItems = document.querySelector('.menu-items');
 
 hamburgerMenu.addEventListener('click', () => {
   menuItems.classList.toggle('active');
 });
+
+//! CARDS
+document.querySelectorAll('.arrow').forEach(function(arrow) {
+  arrow.addEventListener('click', function() {
+    var descricao = this.parentNode.querySelector('.descricao');
+    if(descricao.style.display === 'block') {
+      descricao.style.display = 'none';
+    } else {
+      descricao.style.display = 'block';
+    }
+  });
+});
+
+//! arrow
+document.addEventListener('DOMContentLoaded', function() {
+  const arrowIcon = document.getElementById('arrowIcon');
+
+    arrowIcon.addEventListener('click', function() {
+      const iconElement = arrowIcon.querySelector('i');
+
+        if (iconElement.classList.contains('bxs-chevron-down')) {
+          iconElement.classList.remove('bxs-chevron-down');
+          iconElement.classList.add('bxs-chevron-up');
+        } else {
+          iconElement.classList.remove('bxs-chevron-up');
+          iconElement.classList.add('bxs-chevron-down');
+        }
+    });
+});
+
