932995d30d58a8fbb34697d2de227637c872fb37
Lucas Rocha <108487239+lucasrochapng@users.noreply.github.com>
2023-12-25 16:49:32 -0300

    att

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const menuItems = document.querySelector('.menu-items');
+
+hamburgerMenu.addEventListener('click', () => {
+  menuItems.classList.toggle('active');
+});
