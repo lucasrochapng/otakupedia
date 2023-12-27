1988e83ac810bed274d064d38b320818b791d566
lucas <lucasrochadss@gmail.com>
2023-12-27 12:57:41 -0300

    att filtro por ano ou avaliação

--- js/script.js
+++ js/script.js
@@ -44,44 +44,22 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 //! ordena os cards por ano
-// Obtém todos os elementos de cards
-const cardsContainer = document.querySelector('.cards');
-const cards = Array.from(cardsContainer.children);
+// const cardsContainer = document.querySelector('.cards');
+// const cards = Array.from(cardsContainer.children);
 
-// Ordena os cards com base nos IDs que contêm anos
-cards.sort((a, b) => {
-  const yearA = parseInt(a.id);
-  const yearB = parseInt(b.id);
-    return yearA - yearB;
-});
-
-// Remove os cards existentes do DOM
-while (cardsContainer.firstChild) {
-    cardsContainer.removeChild(cardsContainer.firstChild);
-}
-
-// Adiciona os cards ordenados de volta ao DOM
-cards.forEach(card => cardsContainer.appendChild(card));
-
-//! filtro de pesquisa
-// var inputBuscar = document.getElementById('buscar');
-// inputBuscar.addEventListener('input', function() {
-
-//     var termoPesquisa = inputBuscar.value.toLowerCase();
-//     var cards = document.querySelectorAll('.cards .card');
-
-//     cards.forEach(function(card) {
-//         var h2Element = card.querySelector('h2');
-//         var nome = h2Element.textContent.toLowerCase();
-
-//         if (nome.includes(termoPesquisa)) {
-//             card.style.display = 'block';
-//         } else {
-//             card.style.display = 'none';
-//         }
-//     });
+// cards.sort((a, b) => {
+//   const yearA = parseInt(a.id);
+//   const yearB = parseInt(b.id);
+//     return yearA - yearB;
 // });
 
+// while (cardsContainer.firstChild) {
+//     cardsContainer.removeChild(cardsContainer.firstChild);
+// }
+
+// cards.forEach(card => cardsContainer.appendChild(card));
+
+//! filtro de pesquisa
 var inputBuscar = document.getElementById('buscar');
 
 inputBuscar.addEventListener('input', function() {
@@ -141,7 +119,212 @@ observer.observe(targetNode, config);
 // Chamada inicial para atualizar as estrelas baseadas nos valores iniciais
 atualizarEstrelaParaTodos();
 
+//! ordena por ano ou por nota
+// document.addEventListener('DOMContentLoaded', function() {
+//   const cardsContainer = document.querySelector('.cards');
+//   const cards = Array.from(cardsContainer.children);
+//   let currentSorting = 'ano'; // Variável para controlar o tipo atual de ordenação
+
+//   function ordenarPorAno() {
+//       cards.sort((a, b) => {
+//           const yearA = parseInt(a.id);
+//           const yearB = parseInt(b.id);
+//           return yearA - yearB;
+//       });
+
+//       atualizarCards();
+//   }
+
+//   function ordenarPorRating() {
+//       cards.sort((a, b) => {
+//           const ratingA = parseFloat(a.querySelector('#number').textContent);
+//           const ratingB = parseFloat(b.querySelector('#number').textContent);
+//           return ratingB - ratingA;
+//       });
+
+//       atualizarCards();
+//   }
+
+//   function atualizarCards() {
+//       while (cardsContainer.firstChild) {
+//           cardsContainer.removeChild(cardsContainer.firstChild);
+//       }
+
+//       cards.forEach(card => cardsContainer.appendChild(card));
+//   }
+
+//   function mostrarCardsOrdenados() {
+//       if (currentSorting === 'ano') {
+//           ordenarPorAno();
+//       } else if (currentSorting === 'rating') {
+//           ordenarPorRating();
+//       }
+//   }
+
+//   // Evento de clique no ícone de filtro
+//   const filtroIcon = document.querySelector('.escolher-filtro i');
+//   filtroIcon.addEventListener('click', () => {
+//       const opcoesFiltro = document.createElement('div');
+//       opcoesFiltro.classList.add('opcoes-filtro');
+
+//       const filtroPorAno = document.createElement('div');
+//       filtroPorAno.textContent = 'Filtrar por Ano';
+//       filtroPorAno.addEventListener('click', () => {
+//           currentSorting = 'ano';
+//           mostrarCardsOrdenados();
+//           opcoesFiltro.remove();
+//       });
+
+//       const filtroPorRating = document.createElement('div');
+//       filtroPorRating.textContent = 'Filtrar por Rating';
+//       filtroPorRating.addEventListener('click', () => {
+//           currentSorting = 'rating';
+//           mostrarCardsOrdenados();
+//           opcoesFiltro.remove();
+//       });
+
+//       opcoesFiltro.appendChild(filtroPorAno);
+//       opcoesFiltro.appendChild(filtroPorRating);
+
+//       const filtroAberto = document.querySelector('.opcoes-filtro');
+//       if (filtroAberto) {
+//           filtroAberto.remove();
+//       } else {
+//           filtroIcon.parentElement.appendChild(opcoesFiltro);
+//       }
+//   });
+
+//   // Inicialmente, ordenar os cards por ano
+//   mostrarCardsOrdenados();
+// });
 
 
 
 
+
+document.addEventListener('DOMContentLoaded', function() {
+  const cardsContainer = document.querySelector('.cards');
+  const cards = Array.from(cardsContainer.children);
+  let currentSorting = 'ano'; // Variável para controlar o tipo atual de ordenação
+
+  function ordenarPorAno() {
+    cards.sort((a, b) => {
+      const yearA = parseInt(a.id);
+      const yearB = parseInt(b.id);
+      return yearA - yearB;
+    });
+    
+    atualizarCards();
+  }
+
+  function ordenarPorRating() {
+    cards.sort((a, b) => {
+      const ratingA = parseFloat(a.querySelector('#number').textContent);
+      const ratingB = parseFloat(b.querySelector('#number').textContent);
+      return ratingB - ratingA;
+    });
+      
+    atualizarCards();
+  }
+
+  function atualizarCards() {
+    while (cardsContainer.firstChild) {
+      cardsContainer.removeChild(cardsContainer.firstChild);
+    }
+    
+    cards.forEach(card => cardsContainer.appendChild(card));
+  }
+
+  function mostrarCardsOrdenados() {
+    if (currentSorting === 'ano') {
+      ordenarPorAno();
+    } else if (currentSorting === 'rating') {
+      ordenarPorRating();
+    }
+  }
+
+  function limparSelecaoFiltros() {
+    const filtros = document.querySelectorAll('.opcao-filtro');
+    filtros.forEach(filtro => {
+        filtro.classList.remove('filtro-selecionado');
+    });
+  }
+
+  // Ícones
+  const iconAno = document.createElement('i');
+  iconAno.classList.add('bx', 'bxs-calendar');
+
+  const iconRating = document.createElement('i');
+  iconRating.classList.add('bx', 'bxs-star');
+
+
+  // Criando a janela modal
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+  modal.style.display = 'none'; // Inicialmente, a janela modal estará oculta
+
+  
+
+  //const opcaoAno = document.createElement('div');
+  //opcaoAno.textContent = 'Filtrar por ano';
+  //opcaoAno.addEventListener('click', () => {
+  //    currentSorting = 'ano';
+  //    mostrarCardsOrdenados();
+  //    modal.style.display = 'none'; // Esconde a janela modal após selecionar uma opção
+  //});
+  const opcaoAno = document.createElement('div');
+  opcaoAno.classList.add('opcao-filtro');
+  opcaoAno.appendChild(iconAno); // Adiciona o ícone ao lado da frase
+  opcaoAno.innerHTML += 'mais antigo';
+  opcaoAno.classList.add('filtro-selecionado'); //! se der erro no filtro, remover essa linha
+  opcaoAno.addEventListener('click', () => {
+    currentSorting = 'ano';
+    mostrarCardsOrdenados();
+    modal.style.display = 'none'; // Esconde a janela modal após selecionar uma opção
+    limparSelecaoFiltros();
+    opcaoAno.classList.add('filtro-selecionado');
+  });
+
+  //const opcaoRating = document.createElement('div');
+  //opcaoRating.textContent = 'Filtrar por nota';
+  //opcaoRating.addEventListener('click', () => {
+  //    currentSorting = 'rating';
+  //    mostrarCardsOrdenados();
+  //    modal.style.display = 'none'; // Esconde a janela modal após selecionar uma opção
+  //});
+  const opcaoRating = document.createElement('div');
+  opcaoRating.classList.add('opcao-filtro');
+  opcaoRating.appendChild(iconRating); // Adiciona o ícone ao lado da frase
+  opcaoRating.innerHTML += 'melhor avaliação';
+  opcaoRating.addEventListener('click', () => {
+    currentSorting = 'rating';
+    mostrarCardsOrdenados();
+    modal.style.display = 'none'; // Esconde a janela modal após selecionar uma opção
+    limparSelecaoFiltros();
+    opcaoRating.classList.add('filtro-selecionado');
+  });
+
+
+//   // Adicionando os ícones aos elementos de filtro
+// opcaoAno.insertAdjacentElement('afterbegin', iconAno); // Adiciona o ícone antes do texto
+// opcaoRating.insertAdjacentElement('afterbegin', iconRating); 
+
+
+  modal.appendChild(opcaoAno);
+  modal.appendChild(opcaoRating);
+
+  // Adicionando a janela modal ao final do body
+  document.body.appendChild(modal);
+
+  // Evento de clique no ícone de filtro
+  const filtroIcon = document.querySelector('.escolher-filtro i');
+  let modalVisivel = false; // Variável para controlar o estado da janela modal
+
+  filtroIcon.addEventListener('click', () => {
+      modalVisivel = !modalVisivel; // Alterna o estado da janela modal
+      modal.style.display = modalVisivel ? 'block' : 'none'; // Exibe ou oculta a janela modal
+  });
+
+  // Inicialmente, ordenar os cards por ano
+  mostrarCardsOrdenados();
+});
