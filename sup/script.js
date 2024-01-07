d0de6b23f6b30a7989413e6f09d939d4e72d81c4
lucas <lucasrochadss@gmail.com>
2024-01-07 11:18:46 -0300

    att

--- sup/script.js
+++ sup/script.js
@@ -1,24 +1,3 @@
-// function sendMessage() {
-//     var message = document.getElementById("message-input").value;
-//     var chatBox = document.getElementById("chat-box");
-  
-//     if (message !== "") {
-//       var newMessage = document.createElement("p");
-//       newMessage.innerText = `${message}  ${getCurrentTime()}`; // Adicionando o horário atual
-//       newMessage.classList.add("sent-message"); // Adicionando a classe para mensagem enviada
-//       chatBox.appendChild(newMessage);
-//       document.getElementById("message-input").value = "";
-//       chatBox.scrollTop = chatBox.scrollHeight;
-//     }
-//   }
-  
-//   function getCurrentTime() {
-//     var now = new Date();
-//     var hours = now.getHours().toString().padStart(2, '0');
-//     var minutes = now.getMinutes().toString().padStart(2, '0');
-//     return hours + ':' + minutes;
-//   }
-
 function sendMessage() {
     var message = document.getElementById("message-input").value;
     var chatBox = document.getElementById("chat-box");
